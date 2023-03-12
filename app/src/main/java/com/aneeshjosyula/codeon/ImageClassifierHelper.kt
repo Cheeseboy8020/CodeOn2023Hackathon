8cea669b79a1523213569b80fd912a2f30fba79a
cheeseboy <aneeshjosyula@gmail.com>
2023-03-12 15:40:33 -0400

    AI is working

--- app/src/main/java/com/aneeshjosyula/codeon/ImageClassifierHelper.kt
+++ app/src/main/java/com/aneeshjosyula/codeon/ImageClassifierHelper.kt
@@ -1,19 +1,6 @@
 /*
- * Copyright 2022 The TensorFlow Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *             http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+Credit: Tensorflow Authors
  */
-
 package com.aneeshjosyula.codeon
 
 import android.content.Context
@@ -137,15 +124,6 @@ class ImageClassifierHelper(
                 return ImageProcessingOptions.Orientation.RIGHT_TOP
         }
     }
-
-    interface ClassifierListener {
-        fun onError(error: String)
-        fun onResults(
-            results: List<Classifications>?,
-            inferenceTime: Long
-        )
-    }
-
     companion object {
         const val DELEGATE_CPU = 0
         const val DELEGATE_GPU = 1
